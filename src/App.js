5d982aef1fe054d84acd0ab4181e306e36333e05
ignas26 <ignas.zlatkus@gmail.com>
2018-06-14 02:06:10 +0300

    updated with additional components(statistics, orders, settings) and correspondent css components. Created renderContent method to switch between components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import Settings from './components/Settings';
+import Statistics from './components/Statistics';
+import Orders from './components/Orders';
 
 class App extends Component {
   state={
@@ -11,6 +14,14 @@ class App extends Component {
     this.setState({activeTab:i})
   };
 
+  renderContent = ()=>{
+    switch (this.state.activeTab){
+      case 0 : return <Orders/>;
+      case 1 : return <Statistics/>;
+      case 2 : return <Settings/>;
+      default: return null;
+    }
+  };
 
   render() {
     return (
@@ -20,6 +31,7 @@ class App extends Component {
               switchTab={this.switchTab}
               tabs={this.state.tabs}/>
           <Menu/>
+          {this.renderContent()}
         </div>
     );
   }
