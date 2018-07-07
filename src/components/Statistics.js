641c146a7c557b972a2902a20e0506e7e12b9eb5
ignas26 <ignas.zlatkus@gmail.com>
2018-07-07 18:54:00 +0300

    updated stats components with the sparklines library, additional css added for menu-items and header nav

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 const Statistics = ({completed})=>{
 
+
   const total = completed.reduce((total,a)=>total+a.price, 0);
 
+ var arr=[];
+ arr=[...completed].map(item=>{
+    return item.price
+  });
+  console.log(arr);
+
   return (
-      <div>
-        Statistics
+      <div className="stats">
+
         <h1>Total: {total.toFixed(2)}</h1>
+        <div className="sparklines">
+          <Sparklines data={arr}>
+            <SparklinesLine color="green" />
+          </Sparklines>
+        </div>
       </div>
   );
 };
